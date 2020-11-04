40374f777df90d6251157114ed0ecf9cdaeb7876
Constantin Amundsen <constantin.amundsen@home24.de>
2020-11-04 12:14:59 +0100

    added functionality for diaplying data with single and endless scann functions

--- csvRead.go
+++ csvRead.go
@@ -3,11 +3,14 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"log"
 	"os"
+	"time"
 )
 
-func csvRead(code string) {
+func csvRead(code string, option string) {
 	file, err := os.OpenFile("data/testDatabase.csv", os.O_RDWR|os.O_CREATE, 0755)
+	defer file.Close()
 	check(err)
 	reader := csv.NewReader(file)
 	records, err := reader.ReadAll()
@@ -16,9 +19,21 @@ func csvRead(code string) {
 	// If the code matches an entry in the Databease, show the data. Else return an error.
 	for index, record := range records {
 		if record[0] == code {
-			fmt.Println("Nr.: ", index, " == ", record[0], " == ", record[1], " == ", record[2], "\nDescription: ", record[3])
+			fmt.Println("Nr.: ", index,
+				" == ", record[0],
+				" == ", record[1],
+				" == ", record[2],
+				"\nDescription: ", record[3],
+				"\n========================================================")
+		} else if code == "end" {
+			log.Println("Scanned end/exit code, exiting!\nBye!")
+			os.Exit(0)
 		} else if index + 2 > len(records) && record[0] != code {
 			fmt.Println("This code is not stored in the system.")
 		}
+	}
+
+	if option != "5" && option != "6" {
+		time.Sleep(time.Second*4)
 	}
 }
--- main.go
+++ main.go
@@ -18,23 +18,30 @@ func check(err error) {
 func main() {
 	initMenus()
 	scanner := bufio.NewScanner(os.Stdin)
-	mainM := menustyling.GetStoredMenu("main")
-	mainM.DisplayMenu()
+	mainMenu := menustyling.GetStoredMenu("main")
 
-	switch mainM.GetInputData() {
-	case "1": // Get Data of one Entry
-		fmt.Println("Please enter or scan a code.")
-		scanner.Scan()
-		csvRead(scanner.Text())
-	case "2": // Edit one Entry based on Barcode
-	case "3": // Delete one Entry based on Barcode
-	case "4": // Add one Entry
-	case "5": // Get data from endless codes, terminate with strg+c or "end" code
-	case "6": // Add endless entries, terminate with strg+c or "end" code
-	case "q": // Quit programm
-		log.Println("pressed exit, programm Exiting.\nBye!")
-		os.Exit(0)
-	default:
-		main()
+	for true {
+		mainMenu.DisplayMenu()
+		switch mainMenu.GetInputData() {
+		case "1": // Get Data of one Entry
+			fmt.Println("Please enter or scan a code.")
+			scanner.Scan()
+			csvRead(scanner.Text(), mainMenu.GetInputData())
+		case "2": // Edit one Entry based on Barcode
+		case "3": // Delete one Entry based on Barcode
+		case "4": // Add one Entry
+		case "5": // Get data from endless codes, terminate with strg+c or "end" code
+			for true{
+				scanner.Scan()
+				csvRead(scanner.Text(), mainMenu.GetInputData())
+			}
+		case "6": // Add endless entries, terminate with strg+c or "end" code
+		case "q": // Quit programm
+			log.Println("pressed exit, programm Exiting.\nBye!")
+			os.Exit(0)
+		default:
+			continue
+		}
 	}
+
 }
