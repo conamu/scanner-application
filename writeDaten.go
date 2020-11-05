8095790051f035ebd9d5ceda7d60d47740bbc669
AnastasiiaShelemeteva <williamhyde752@gmail.com>
2020-11-05 14:16:34 +0100

    Try to connect Adding and Changing artikel

--- writeDaten.go
+++ writeDaten.go
@@ -10,60 +10,86 @@ import (
 	"strings"
 )
 
-func writeDaten() {
+func writeDaten(data []string) {
 
-	var path = "testDatabase.csv"
+	fmt.Println(data)
+	var path = "data/testDatabase.csv"
 	//open a file with flags: to append (O_Append) and to write(O_WRONLY)
 	//FileMode (permission) - to append only
-	file, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_RDWR, os.ModeAppend)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	check(err)
+
 	//creating a two-demensional slice, where we gonna save input-sclices
 	var products [][]string
 	scanner := bufio.NewScanner(os.Stdin)
 
-	fmt.Print("Please, scan a barcode: ")
-	scanner.Scan()
-	barcode := scanner.Text()
+	if len(data) == 0 {
 
-	fmt.Print("Please, write a name (max. 150 character): ")
-	scanner.Scan()
-	name := scanner.Text()
-	name = charLimiter(name, 150)
+		fmt.Print("Please, scan a barcode: ")
+		scanner.Scan()
+		barcode := scanner.Text()
 
-	fmt.Print("Please, write a category (max. 20 character): ")
-	scanner.Scan()
-	category := scanner.Text()
-	category = charLimiter(category, 20)
+		fmt.Print("Please, write a name (max. 150 character): ")
+		scanner.Scan()
+		name := scanner.Text()
+		name = charLimiter(name, 150)
 
-	fmt.Print("Please, write a description (max. 500 character): ")
-	scanner.Scan()
-	description := scanner.Text()
-	description = charLimiter(description, 500)
+		fmt.Print("Please, write a category (max. 20 character): ")
+		scanner.Scan()
+		category := scanner.Text()
+		category = charLimiter(category, 20)
 
-	//creating a slice "product", which holds input as it's elements
-	product := []string{barcode, name, category, description}
+		fmt.Print("Please, write a description (max. 500 character): ")
+		scanner.Scan()
+		description := scanner.Text()
+		description = charLimiter(description, 500)
 
-	//adding a slice "product" to two-dementtional slice "products"
-	products = append(products, product)
+		//creating a slice "product", which holds input as it's elements
+		product := []string{barcode, name, category, description}
 
-	//creating a new writer, that will write to our csv file)
-	writer := csv.NewWriter(file)
-	writer.WriteAll(products)
+		//appending a slice "product" to two-dementtional slice "products"
+		products = append(products, product)
+		fmt.Println(products, "if we are here its good")
+		writer := csv.NewWriter(file)
+		writer.WriteAll(products)
 
-	if err := writer.Error(); err != nil {
-		log.Fatal(err)
+		if err := writer.Error(); err != nil {
+			log.Fatal(err)
+		}
+		writer.Flush()
+	} else {
+
+		fmt.Println(data, "else")
+		for _, value := range records {
+			fmt.Println(data, "for")
+			if value[0] == data[0] {
+				fmt.Println(data, "if")
+				deleteData(value[0])
+				writer := csv.NewWriter(file)
+				writer.Write(data)
+				if err := writer.Error(); err != nil {
+					log.Fatal(err)
+				}
+				writer.Flush()
+			}
+		}
+
 	}
-
 }
+
+//creating a new writer, that will write into our csv file)
 
 func charLimiter(s string, limit int) string {
 	//create a new reader, that is gonna read through s string
 	reader := strings.NewReader(s)
-	//create a buffer, who's size gonna be limited
+	//create a buffer (slice of bytes), who's size gonna be limited
 	buff := make([]byte, limit)
 	//using ReadAtLeast we gonna read (s) into buff until it has read at least minimum byte (limit)
 	//it will read also futher, but buff is limited by (limit) and it will not take more characters than that
