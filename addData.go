4380724927c16886417d7848cb315376016aab74
Constantin Amundsen <constantin.amundsen@gmail.com>
2020-11-10 13:18:20 +0100

    tidied up

--- addData.go
+++ addData.go
@@ -53,7 +53,7 @@ func writeDaten(data []string) bool {
 	records, err := reader.ReadAll()
 	check(err)
 
-	//creating a two-demensional slice, where we gonna save input-sclices
+	//creating a two-dimensional slice, where we save input-slices
 	var products [][]string
 
 	barcode := ""
@@ -61,7 +61,6 @@ func writeDaten(data []string) bool {
 	if len(data) == 0 {
 
 		barcode, name, category, description := getParams()
-
 		if barcode == "end" {
 			return false
 		}
@@ -80,21 +79,20 @@ func writeDaten(data []string) bool {
 			return false
 		}
 
-	fmt.Print("Whats the Product name? (max. 150 characters): ")
-	scanner.Scan()
-	name := scanner.Text()
-	name = charLimiter(name, 150)
-
+		fmt.Print("Whats the Product name? (max. 150 characters): ")
+		scanner.Scan()
+		name := scanner.Text()
+		name = charLimiter(name, 150)
 
 		for _, value := range records {
 
 			if value[0] == data[0] {
 
-	fmt.Print("Whats the Product description? (max. 500 characters): ")
-	scanner.Scan()
-	description := scanner.Text()
-	description = charLimiter(description, 500)
-	fmt.Println("====================================================")
+				fmt.Print("Whats the Product description? (max. 500 characters): ")
+				scanner.Scan()
+				description := scanner.Text()
+				description = charLimiter(description, 500)
+				fmt.Println("====================================================")
 
 				products = append(products, data)
 				writer := csv.NewWriter(file)
@@ -103,12 +101,10 @@ func writeDaten(data []string) bool {
 					log.Fatal(err)
 				}
 				writer.Flush()
-
 			}
-
 		}
-
 	}
+
 	//creating a new writer, that will write into our csv file)
 	writer := csv.NewWriter(file)
 	writer.WriteAll(products)
@@ -122,9 +118,9 @@ func writeDaten(data []string) bool {
 
 func writeKvData() {
 
-	barcode, name, category, description := getParams()
+	// barcode, name, category, description := getParams()
 
-	
+
 
 	// Initialize a Read-Write Transaction
 	err := db.Update(func(txn *badger.Txn) error {
@@ -133,13 +129,29 @@ func writeKvData() {
 		defer txn.Discard()
 
 		// Use the Transaction
-		err :=  txn.Set([]byte("test"), []byte("This is a test Value"))
+		err := txn.Set([]byte("test"), []byte("This is a test Value"))
+		check(err)
+		err = txn.Set([]byte("test"), []byte("This is another test value."))
+		check(err)
 		err = txn.Commit()
 		check(err)
 
 		return err
 	})
 	check(err)
+
+	db.View(func(txn *badger.Txn) error {
+
+		txn = db.NewTransaction(false)
+		item, _ := txn.Get([]byte("test"))
+
+		value, _ := item.ValueCopy(nil)
+		fmt.Println(string(value))
+
+		return nil
+	})
+
+
 }
 
 func charLimiter(s string, limit int) string {
